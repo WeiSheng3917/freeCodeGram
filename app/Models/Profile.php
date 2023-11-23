1d640b668e38f26fa9cbf4b9376ec597bc3f8148
WeiSheng3917 <lwsnumber1@gmail.com>
2023-11-23 18:35:23 +0800

    initial commit

---
+++ app/Models/Profile.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Profile extends Model
+{
+    use HasFactory;
+
+    protected $guarded = [];
+
+    public function profileImage()
+    {
+        $imagePath = ($this->image)
+            ? $this->image
+            : 'profile/LK3TWQpdUIOUO0PYptD8XvvIZlyrt1OXhEYeH0AZ.png';
+
+        return '/storage/' . $imagePath;
+    }
+
+    public function followers()
+    {
+        return $this->belongsToMany(User::class);
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+}
